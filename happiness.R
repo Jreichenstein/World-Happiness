b9c7ce631f233dba0c96fb2155a13d90725cdac2
Jreichenstein <jreichenstein@gmail.com>
2018-04-23 17:31:23 -0700

    taking out "rank" from cor

--- happiness.R
+++ happiness.R
@@ -4,11 +4,12 @@ library(corrplot)
 #Reading in World Happiness data to "happy"
 happy <- read_csv("2017.csv")
 
-#creating tibble with only numerical columns
-happy_nums <- happy[,2:12]
+#creating tibble with only numerical columns (minus rank)
+happy_nums <- happy[,3:12]
 
 #generating correlation matrix
 corrMat <- cor(happy_nums)
 
 #Correlation plot
 corrplot(corrMat, method = "ellipse")
+
